1d8f1bbf93fe0acbb0384a3d95b61b2e2652b4eb
Stephen Johnson <stephenjayyy@gmail.com>
2021-08-30 16:00:44 -0500

    Adds backing var for twitter address

--- SchoolLibrary/School.cs
+++ SchoolLibrary/School.cs
@@ -4,11 +4,30 @@ namespace SchoolLibrary
 {
     class School
     {
-        string SchoolName;
-        string SchoolAddress;
-        string SchoolCity;
-        string SchooleState;
-        string SchoolZip;
-        string PhoneNumber;
+        string Name { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+        string PhoneNumber { get; set; }
+
+
+        private string _twitterAddress;
+        public string TwitterAddress
+        {
+            get { return TwitterAddress; }
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                }
+                else
+                {
+                    throw new Exception("the twitter address must begin with @");
+                }
+            }
+
+        }
     }
 }
