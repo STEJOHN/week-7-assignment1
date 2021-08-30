f57b70fa92bbac581410143f6649da59cc7089cb
Stephen Johnson <stephenjayyy@gmail.com>
2021-08-30 13:40:49 -0500

    Initial commit

---
+++ SchoolLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
