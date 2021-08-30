3096bb54cf270fa4008c8c6c2d07f08ee6a793d3
Stephen Johnson <stephenjayyy@gmail.com>
2021-08-30 15:49:35 -0500

    Added new classes

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string SchoolName;
+        string SchoolAddress;
+        string SchoolCity;
+        string SchooleState;
+        string SchoolZip;
+        string PhoneNumber;
+    }
+}
